52ba132aab4719094da0d03343a033396dd56158
Pratul dawande <>
2026-03-09 21:37:43 +0530

    added config example file

---
+++ aws-exports.example.js
@@ -0,0 +1,10 @@
+// This is an how the awsconfig object should look.
+
+const awsconfig = {
+  aws_appsync_graphqlEndpoint: '',
+  aws_appsync_region: '',
+  aws_appsync_authenticationType: '',
+  aws_appsync_apiKey: '',
+};
+
+export default awsconfig;
